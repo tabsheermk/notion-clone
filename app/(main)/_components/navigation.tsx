a398ece9a931d921cd6bdd4b7822fa10bbdbeac0
Mohd Khaja Tabsheeruddin <creativemk12@gmail.com>
2023-10-13 10:18:10 +0530

    9-[Documents list component created]

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -17,12 +17,12 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { useMutation } from "convex/react";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -144,9 +144,7 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
